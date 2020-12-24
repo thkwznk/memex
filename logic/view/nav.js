b3985c5ac7fd82320a9bab99e702383dac8f9d38
thkwznk <kac.wozniak@gmail.com>
2020-12-25 00:55:54 +0100

    Remove the add script

--- logic/view/nav.js
+++ logic/view/nav.js
@@ -1,24 +1,12 @@
-function Nav()
-{
+function Nav() {
   this.container = null;
 
-  this.install = function(container)
-  {
+  this.install = function (container) {
     this.container = container;
-  }
+  };
 
-  this.display = function(value)
-  {
+  this.display = function (value) {
     let navContent = ``;
-    if (window.showAdd !== undefined && window.showAdd)
-    {
-      // ADD
-      navContent += `<div class="nav-itemgroup">`;
-      navContent += `<a href='#add' class="nav-item">`;
-      navContent += `<b>a</b>dd`;
-      navContent += `</a>`;
-      navContent += `</div>`;
-    }
 
     // TOTAL
     navContent += `<div class="nav-itemgroup">`;
@@ -29,23 +17,27 @@ function Nav()
     navContent += `</div>`;
 
     // DONE
-    if (SETTINGS.SHOWDONE)
-    {
+    if (SETTINGS.SHOWDONE) {
       navContent += `<div class="nav-itemgroup">`;
       navContent += `<a href='#done-true' class="nav-item">`;
       navContent += `<div class="nav-itemcount">${value.done}</div>`;
-      navContent += `<i title="done" class="nav-itemicon ${Icons['true']}"></i>`;
+      navContent += `<i title="done" class="nav-itemicon ${Icons["true"]}"></i>`;
       navContent += `</a>`;
       navContent += `<a href='#done-false' class="nav-item">`;
-      navContent += `<div class="nav-itemcount">${value.total - value.done}</div>`;
-      navContent += `<i title="to do" class="nav-itemicon ${Icons['false']}"></i>`;
+      navContent += `<div class="nav-itemcount">${
+        value.total - value.done
+      }</div>`;
+      navContent += `<i title="to do" class="nav-itemicon ${Icons["false"]}"></i>`;
       navContent += `</a>`;
       navContent += `</div>`;
     }
 
     navContent += `<div class="nav-itemgroup">`;
-    for (let ty = 0; ty < Math.min(value.types.length, SETTINGS.STATSNUMTYPE); ty++) 
-    {
+    for (
+      let ty = 0;
+      ty < Math.min(value.types.length, SETTINGS.STATSNUMTYPE);
+      ty++
+    ) {
       const type = value.types[ty][0];
       const count = value.types[ty][1];
       const icon = Icons[type];
@@ -58,8 +50,7 @@ function Nav()
 
     // TERM
     navContent += `<div class="nav-itemgroup">`;
-    if (value.terms > 0)
-    {
+    if (value.terms > 0) {
       navContent += `<a href='#term' class="nav-item">`;
       navContent += `<div class="nav-itemcount">${value.terms}</div>`;
       navContent += `<i title="terms" class="nav-itemicon fas fa-ribbon"></i>`;
@@ -69,12 +60,14 @@ function Nav()
 
     // TAGS
     navContent += `<div class="nav-itemgroup">`;
-    if (value.tags.length > 0)
-    {
+    if (value.tags.length > 0) {
       navContent += `<div class="nav-tagcontainer">`;
       navContent += `<i title="tags" class="nav-tagicon fas fa-tag"></i>`;
-      for (var t = 0; t < Math.min(value.tags.length, SETTINGS.STATSNUMTAGS); t++) 
-      {
+      for (
+        var t = 0;
+        t < Math.min(value.tags.length, SETTINGS.STATSNUMTAGS);
+        t++
+      ) {
         navContent += `<a class="nav-tag" href='#tag-${value.tags[t][0]}'>`;
         navContent += `<div class="nav-tagcount">${value.tags[t][1]}</div>`;
         navContent += `<div class="nav-taglabel">${value.tags[t][0]}</div>`;
@@ -84,5 +77,5 @@ function Nav()
     }
     navContent += `</div>`;
     this.container.innerHTML = navContent;
-  }
+  };
 }
