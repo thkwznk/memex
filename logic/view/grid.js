1e447772aed406b1d6b12f26be6509c60d832df0
thkwznk <kac.wozniak@gmail.com>
2020-12-30 01:26:40 +0100

    Refactor Grid class to create Nodes directly

--- logic/view/grid.js
+++ logic/view/grid.js
@@ -1,3 +1,38 @@
+function createElement(elementTag, options = {}, ...children) {
+  const element = document.createElement(elementTag);
+
+  for (let option in options) {
+    if (element[option] !== undefined) element[option] = options[option];
+    else element.setAttribute(option, options[option]);
+  }
+
+  for (let child of children.flat(2)) {
+    if (!child) continue;
+
+    element.appendChild(
+      typeof child === "string"
+        ? createElement("span", { innerText: child })
+        : child
+    );
+  }
+
+  return element;
+}
+
+const Icon = (type, label, altClass) =>
+  createElement("i", {
+    title: label || type || undefined,
+    className: [Icons[type], "textIcon", altClass || "article-icon"].join(" "),
+  });
+
+const Row = ({ className, type }, ...children) =>
+  createElement(
+    "div",
+    { className: `article-row ${className}` },
+    type && Icon(type),
+    createElement("div", { className: "article-rowtext" }, children)
+  );
+
 class Grid {
   constructor({ container, overlay, numberOfColumns = 3 }) {
     this.util = new Util();
@@ -6,9 +41,7 @@ class Grid {
     this.columns = [];
 
     for (let i = 0; i < numberOfColumns; i++) {
-      let column = document.createElement("div");
-      column.className = "column";
-
+      let column = createElement("div", { className: "column" });
       this.container.appendChild(column);
       this.columns.push(column);
     }
@@ -22,333 +55,279 @@ class Grid {
     this.clear();
 
     for (let i = 0; i < articles.length; i++) {
-      this.columns[i % this.columns.length].appendChild(
-        this.createElementFromHTML(articles[i])
-      );
+      this.columns[i % this.columns.length].appendChild(articles[i]);
     }
 
     seer.note("render html");
   }
 
-  createElementFromHTML(htmlString) {
-    var div = document.createElement("div");
-    div.innerHTML = htmlString.trim();
-
-    return div.firstChild;
-  }
-
   buildAllArticles(db) {
-    return Object.keys(db).map((dbKey) => this.buildArticle(db[dbKey], dbKey));
+    return Object.keys(db).map((dbKey) => this.buildArticle(dbKey, db[dbKey]));
   }
 
-  buildArticle(value, key) {
-    let itemClass = "article";
-
+  buildArticle(key, value) {
     let isImageType =
-      SETTINGS.SHOWIMAG && this.util.isType(value.TYPE, "image");
+      SETTINGS.SHOWIMAG &&
+      this.util.isType(value.TYPE, "image") &&
+      value.FILE &&
+      this.util.isImage(value.FILE);
 
-    let onclickImage = isImageType
-      ? `onclick="main.grid.handleImageClick(event, this, '${value.FILE}');"`
-      : "";
-
-    // ARTICLE
-    let article = `<article class="${itemClass}" id="${
-      SETTINGS.ARTICLEIDBASE + value.DIID
-    }">`;
-
-    if (value.LINK) {
-      let isUrlSeen = value.SEEN && value.SEEN === "true";
-      var idUrl = isUrlSeen ? "urlseen" : "url";
-
-      // LINK START
-      if (SETTINGS.SHOWLINK && !this.util.isObject(value.LINK)) {
-        // If this item has only one link then make the whole title the link
-        article += `<a class="article-link" href="${String(
-          value.LINK
-        )}" id="${idUrl}">`;
-      }
-    }
-
-    // UPPER CONTENT START
-    if (SETTINGS.SHOWUPPER) {
-      let upperClass = isImageType
-        ? "article-containerupper-image"
-        : "article-containerupper";
-
-      article += `<div class="${upperClass}" ${onclickImage}>`;
-
-      // TITLE
-      if (SETTINGS.SHOWTITLE) {
-        article += `<header class="article-title">${key.to_properCase()}</header>`;
-      }
-
-      // LINK END
-      if (SETTINGS.SHOWLINK && value.LINK) {
-        if (this.util.isObject(value.LINK)) {
-          for (let l = 0; l < value.LINK.length; l++) {
-            article += `<a class="article-link" href="${String(
-              value.LINK[l]
-            )}" id="${idUrl}">`;
-            article += `<div class="article-linkcontainer"><div class="article-linkicon">${this.util.buildIcon(
-              "link"
-            )}</div><div class="article-linktitle">${this.util.extractRootDomain(
-              value.LINK[l]
-            )}</div></div></a>`;
-          }
-        } else {
-          article += `<div class="article-linkcontainer"><div class="article-linkicon">${this.util.buildIcon(
-            "link"
-          )}</div><div class="article-linktitle">${this.util.extractRootDomain(
-            value.LINK
-          )}</div></div></a>`;
-        }
-      }
-
-      // TYPE
-      if (SETTINGS.SHOWTYPE || SETTINGS.SHOWDONE) {
-        article += `<div class="article-typecontainer">`;
-
-        if (SETTINGS.SHOWTYPE && value.TYPE) {
-          for (let tc = 0; tc < value.TYPE.length; tc++) {
-            article += `<a class="article-type" href='#type-${value.TYPE[tc]}'>`;
-            article += this.util.buildIcon(
-              value.TYPE[tc],
-              value.TYPE[tc],
-              "article-typeicon"
-            );
-            article += `</a>`;
-          }
-        }
-
-        if (SETTINGS.SHOWDONE) {
-          let done = value.DONE || "false";
-          article += `<a class="article-type" href='#done-${done}'>`;
-          article += this.util.buildIcon(done, done, "article-typeicon");
-          article += `</a>`;
-        }
-
-        article += `</div>`;
-      }
-
-      // UPPER CONTENT END
-      article += `</div>`;
-    }
-
-    // IMAGE - for image-type-article
-    if (isImageType && value.FILE && this.util.isImage(value.FILE)) {
-      // IMAGE ARTICLE
-      article += `<div class="article-imageType-imgContainer">`;
-      if (SETTINGS.SHOWOVERLAY) {
-        article += `<div class="image-overlay" ${onclickImage}></div>`;
-      }
-      article += `<img class="article-image-img" src="content/media/${value.FILE}">`;
-
-      article += this.doLower(value, isImageType, onclickImage);
-
-      article += `</div>`;
-
-      article += `<div class="article-containerbelow">`;
-      // TERM
-      if (SETTINGS.SHOWTERM && value.TERM) {
-        article += this.doRowMulti("term", value.TERM);
-      }
-
-      // NOTE
-      if (SETTINGS.SHOWNOTE && value.NOTE) {
-        article += this.doRowMulti("note", value.NOTE);
-      }
-
-      // QUOTE
-      if (SETTINGS.SHOWQOTE && value.QOTE) {
-        article += this.doRowMulti("quote", value.QOTE);
-      }
-
-      // PROGRESS
-      if (SETTINGS.SHOWPROG && value.PROG) {
-        article += this.doRowMulti("progress", value.PROG);
-      }
-      article += `</div>`;
-    } else {
-      // NORMAL ARTICLE (NON-IMAGE)
-      article += this.doLower(value, isImageType, onclickImage);
-    }
-
-    article += `</article>`;
-    return article;
+    return createElement(
+      "article",
+      {
+        className: "article",
+        id: SETTINGS.ARTICLEIDBASE + value.DIID,
+      },
+      SETTINGS.SHOWUPPER &&
+        this.doUpper({
+          className: isImageType
+            ? "article-containerupper-image"
+            : "article-containerupper",
+          onclick: isImageType && this.handleOnClick(value.FILE),
+          value,
+          key,
+        }),
+      isImageType
+        ? this.doImageArticle(value)
+        : this.doLower(value, isImageType)
+    );
   }
 
-  doLower(value, isImageType, onclickImage) {
-    let article = "";
-    // LOWER CONTENT START
-    if (SETTINGS.SHOWLOWER) {
-      let lowerClass = isImageType
-        ? "article-containerlower-image"
-        : "article-containerlower";
+  doImageArticle(value) {
+    return [
+      createElement(
+        "div",
+        {
+          className: "article-imageType-imgContainer",
+        },
+        SETTINGS.SHOWOVERLAY &&
+          createElement("div", {
+            className: "image-overlay",
+            onclick: this.handleOnClick(value.FILE),
+          }),
+        createElement("img", {
+          className: "article-image-img",
+          src: `content/media/${value.FILE}`,
+        }),
+        this.doLower(value, true)
+      ),
+      createElement(
+        "div",
+        { className: "article-containerbelow" },
+        this.doBelow(value)
+      ),
+    ];
+  }
 
-      article += `<div class="${lowerClass}" ${onclickImage}>`;
+  doUpper({ className, onclick, value, key }) {
+    let hasSingleLink =
+      SETTINGS.SHOWLINK && value.LINK && typeof value.LINK !== "object";
+    let links = Array.isArray(value.LINK) ? value.LINK : [value.LINK];
+    let done = value.DONE || "false";
+    let idUrl = value.SEEN && value.SEEN === "true" ? "urlseen" : "url";
 
-      // TIME
-      if (SETTINGS.SHOWDATE && value.DATE) {
-        article += this.doRow("date", value.DATE);
-      }
+    const Header = () =>
+      createElement(
+        "div",
+        {
+          className,
+          onclick,
+        },
+        SETTINGS.SHOWTITLE &&
+          createElement("header", {
+            className: "article-title",
+            innerText: key.to_properCase(),
+          }),
+        SETTINGS.SHOWLINK &&
+          value.LINK &&
+          links.map((link) =>
+            createElement(
+              "a",
+              {
+                className: "article-link",
+                href: String(link),
+                id: idUrl,
+              },
+              createElement(
+                "div",
+                { className: "article-linkcontainer" },
+                createElement(
+                  "div",
+                  { className: "article-linkicon" },
+                  Icon("link")
+                ),
+                createElement("div", {
+                  className: "article-linktitle",
+                  innerText: this.util.extractRootDomain(link),
+                })
+              )
+            )
+          ),
+        (SETTINGS.SHOWTYPE || SETTINGS.SHOWDONE) &&
+          createElement(
+            "div",
+            { className: "article-typecontainer" },
+            SETTINGS.SHOWTYPE &&
+              value.TYPE &&
+              value.TYPE.map((type) =>
+                createElement(
+                  "a",
+                  { className: "article-type", href: `#type-${type}` },
+                  Icon(type, type, "article-typeicon")
+                )
+              ),
+            SETTINGS.SHOWDONE &&
+              createElement(
+                "a",
+                { className: "article-type", href: `#done-${done}` },
+                Icon(done, done, "article-typeicon")
+              )
+          )
+      );
 
-      // AUTHOR
-      if (SETTINGS.SHOWAUTH && value.AUTH) {
-        article += value.AUTH.map((author) =>
-          this.doRow("author", author.to_properCase())
-        ).join("");
-      }
+    // If this item has only one link then make the whole title the link
+    return hasSingleLink
+      ? createElement(
+          "a",
+          {
+            className: "article-link",
+            href: String(value.LINK),
+            id: idUrl,
+          },
+          Header()
+        )
+      : Header();
+  }
 
-      // TAGS
-      if (SETTINGS.SHOWTAGS && value.TAGS) {
-        article += this.doRowArray("tags", value.TAGS, "tag", false);
-      }
+  doLower(value, isImageType) {
+    if (!SETTINGS.SHOWLOWER) return null;
 
-      // PROJECT
-      if (SETTINGS.SHOWPROJ && value.PROJ) {
-        article += this.doRowArray("project", value.PROJ, "proj", true);
-      }
+    let files = Array.isArray(value.FILE) ? value.FILE : [value.FILE];
 
-      if (!isImageType) {
-        // TERM
-        if (SETTINGS.SHOWTERM && value.TERM) {
-          article += this.doRowMulti("term", value.TERM);
-        }
-
-        // NOTE
-        if (SETTINGS.SHOWNOTE && value.NOTE) {
-          article += this.doRowMulti("note", value.NOTE);
-        }
-
-        // QUOTE
-        if (SETTINGS.SHOWQOTE && value.QOTE) {
-          article += this.doRowMulti("quote", value.QOTE);
-        }
-
-        // PROGRESS
-        if (SETTINGS.SHOWPROG && value.PROG) {
-          article += this.doRowMulti("progress", value.PROG);
-        }
-      }
-
+    return createElement(
+      "div",
+      {
+        className: isImageType
+          ? "article-containerlower-image"
+          : "article-containerlower",
+        onclick: isImageType && this.handleOnClick(value.FILE),
+      },
+      SETTINGS.SHOWDATE && value.DATE && Row({ type: "date" }, value.DATE),
+      SETTINGS.SHOWAUTH &&
+        value.AUTH &&
+        value.AUTH.map((author) =>
+          Row({ type: "author" }, author.to_properCase())
+        ),
+      SETTINGS.SHOWTAGS &&
+        value.TAGS &&
+        this.doRowArray("tags", value.TAGS, "tag", false),
+      SETTINGS.SHOWPROJ &&
+        value.PROJ &&
+        this.doRowArray("project", value.PROJ, "proj", true),
+      !isImageType && this.doBelow(value),
       // IMAGE - for non-image-type-article
-      if (
-        SETTINGS.SHOWIMAG &&
+      SETTINGS.SHOWIMAG &&
         !this.util.isType(value.TYPE, "image") &&
         value.FILE &&
-        this.util.isImage(value.FILE)
-      ) {
-        article += `
-        <div class="image">
-          <img class="article-img" src="content/media/${value.FILE}" onclick="lightbox.load('content/media/${value.FILE}')">
-        </div>`;
-      }
-
-      // FILE
-      if (SETTINGS.SHOWFILE && value.FILE) {
-        if (this.util.isObject(value.FILE)) {
-          for (var i = 0; i < value.FILE.length; i++) {
-            article += this.doRow(
-              "file",
-              `<a class="article-file-link" href="content/media/${value.FILE[i]}">${value.FILE[i]}</a>`,
-              "article-file"
-            );
-          }
-        } else {
-          // single
-          article += this.doRow(
-            "file",
-            `<a class="article-file-link" href="content/media/${value.FILE}">${value.FILE}</a>`,
-            "article-file"
-          );
-        }
-      }
-
-      // LOWER CONTENT END
-      article += `</div>`;
-    }
-
-    return article;
+        this.util.isImage(value.FILE) &&
+        createElement(
+          "div",
+          { className: "image" },
+          createElement("img", {
+            className: "article-img",
+            src: `content/media/${value.FILE}`,
+            onclick: this.handleOnClick(value.FILE),
+          })
+        ),
+      SETTINGS.SHOWFILE &&
+        value.FILE &&
+        files.map((file) =>
+          Row(
+            { type: "file", className: "article-file" },
+            createElement(
+              "a",
+              {
+                className: "article-file-link",
+                href: `content/media/${file}`,
+              },
+              file
+            )
+          )
+        )
+    );
   }
 
-  doRow(type, content, extraClass) {
-    return `
-    <div class="article-row ${extraClass}">
-      ${type != undefined ? this.util.buildIcon(type) : ""}
-      <div class="article-rowtext">${content}</div>
-    </div>`;
+  doBelow(value) {
+    return [
+      SETTINGS.SHOWTERM && value.TERM && this.doRowMulti("term", value.TERM),
+      SETTINGS.SHOWNOTE && value.NOTE && this.doRowMulti("note", value.NOTE),
+      SETTINGS.SHOWQOTE && value.QOTE && this.doRowMulti("quote", value.QOTE),
+      SETTINGS.SHOWPROG &&
+        value.PROG &&
+        this.doRowMulti("progress", value.PROG),
+    ];
   }
 
   doRowArray(type, data, query, propercase) {
-    let content = data
-      .map(
-        (value) =>
-          `<a class="article-taglink" href="#${query}-${value}">${
-            propercase ? value.to_properCase() : value
-          }</a>`
-      )
-      .join(", ");
+    let values = data
+      .map((value) => [
+        createElement("a", {
+          className: "article-taglink",
+          href: `#${query}-${value}`,
+          innerText: propercase ? value.to_properCase() : value,
+        }),
+        ", ",
+      ])
+      .flat();
 
-    return this.doRow(type, content);
+    values.pop(); // Remove the last comma
+
+    return Row({ type }, values);
   }
 
   doRowMulti(type, data) {
-    if (!Array.isArray(data)) return this.doRow(type, data);
+    if (!Array.isArray(data)) return Row({ type }, data);
 
-    let result = "";
-
-    for (var value of data) {
-      let prefix = value.substring(0, 2);
-
-      switch (prefix) {
-        // New item
-        case "> ":
-          {
-            if (value.includes(": ")) {
-              let titleSplit = value.substring(2).split(": "); // .substring(2) removes the "> "
-
-              for (var e = 0; e < titleSplit.length; e++) {
-                titleSplit[e] = titleSplit[e].trim();
-              }
-
-              result += this.doRow(
-                type,
-                `<b>${titleSplit[0]}</b>: ${titleSplit[1]}`
-              );
-            } else {
-              result += this.doRow(type, value.substring(2));
-            }
-          }
-          break;
-
-        // New line in current item
-        case "& ":
-          result += this.doRow(null, value.substring(2));
-          break;
-
-        // Bullet point
-        case "- ":
-          result += this.doRow("dash", value.substring(2));
-          break;
-
-        // Handle unformatted
-        default:
-          result += this.doRow(type, value);
-          break;
-      }
-    }
-
-    return result;
+    return data.map((entry) => this.doRowMultiEntry(type, entry));
   }
 
-  handleImageClick(e, element, file) {
-    e = e || window.event;
-    var target = e.target || e.srcElement;
-    if (target == element) {
+  doRowMultiEntry(type, value) {
+    let prefix = value.substring(0, 2);
+
+    // New item
+    if (prefix === "> ") {
+      if (value.includes(": ")) {
+        let titleSplit = value.substring(2).split(": "); // .substring(2) removes the "> "
+
+        return Row(
+          { type },
+          createElement("b", {}, titleSplit[0].trim()),
+          `: ${titleSplit[1].trim()}`
+        );
+      }
+
+      return Row({ type }, value.substring(2));
+    }
+
+    // New line in current item
+    if (prefix === "& ") return Row({ type: null }, value.substring(2));
+
+    // Bullet point
+    if (prefix === "- ") return Row({ type: "dash" }, value.substring(2));
+
+    // Handle unformatted
+    return Row({ type }, value);
+  }
+
+  handleOnClick(file) {
+    return function (e) {
+      e = e || window.event;
+      var target = e.target || e.srcElement;
+
       // If user is clicking given element, or element's background...
       // as opposed to an element's child content, then do lightbox.
       // This stops lightbox from happening when clicking on tags, file etc
-      lightbox.load(`content/media/${file}`);
-    }
+      if (target === this) lightbox.load(`content/media/${file}`);
+    };
   }
 }
